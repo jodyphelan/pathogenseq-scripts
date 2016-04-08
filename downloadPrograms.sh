a519364ed09b21c00b130457033db9654d6c5639
root <root@plum-s1.lshtm.ac.uk>
2016-04-08 16:36:36 +0100

    installation

--- downloadPrograms.sh
+++ downloadPrograms.sh
@@ -15,9 +15,9 @@ unzip Trimmomatic-0.36.zip
 ln -s Trimmomatic-0.36/trimmomatic-0.36.jar trimmomatic.jar
 
 ### GEM
-wget http://master.dl.sourceforge.net/project/gemlibrary/gem-library/Binary%20pre-release%202/GEM-binaries-Linux-x86_64-core_2-20121106-022124.tbz2
-tar -xvf GEM-binaries-Linux-x86_64-core_2-20121106-022124.tbz2 
-ln -s GEM-binaries-Linux-x86_64-core_2-20121106-022124/gem-* .
+wget http://iweb.dl.sourceforge.net/project/gemlibrary/gem-library/Binary%20pre-release%203/GEM-binaries-Linux-x86_64-core_i3-20130406-045632.tbz2 
+tar -xvf GEM-binaries-Linux-x86_64-core_i3-20130406-045632.tbz2 
+ln -s GEM-binaries-Linux-x86_64-core_i3-20130406-045632/gem-* .
 
 ### BEDOPS
 wget https://github.com/bedops/bedops/releases/download/v2.4.16/bedops_linux_x86_64-v2.4.16.tar.bz2
