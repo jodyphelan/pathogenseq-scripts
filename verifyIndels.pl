91e0ef8b1fff3365c8703011df0614eaaff3f3dd
root <root@plum-s1.lshtm.ac.uk>
2016-06-28 18:16:38 +0100

    init

--- verifyIndels.pl
+++ verifyIndels.pl
@@ -204,21 +204,27 @@ my ($base_dir,$sample,$chr,$start,$end) = @_;
 `sambamba view -F "not (unmapped or mate_is_unmapped) and mapping_quality >=30" -o filt.bam $base_dir/bam/$sample.bam $chr:$start-$end -f bam`;
 
 my %assembly;
-
+my %expCov;
+my %covCut;
 for (my $i=49; $i<89; $i=$i+2){
 	my $res = `velvetg test_$i -cov_cutoff auto -exp_cov auto -clean yes| tail -3 | tr '\n' ' '`;
 	$res =~ m/Estimated Coverage = ([\d\.]+).+Estimated Coverage cutoff = ([\d\.]+).+n50 of (\d+)/;
 	my ($exp_cov,$cov_cut,$tempkmer) = ($1,$2,$3);
 	$assembly{$res} = $i;
+	$expCov{$i} = $exp_cov;
+	$covCut{$i} = $cov_cut;
 	print "$i\t$tempkmer\t$exp_cov\t$cov_cut\n";
 } 
-
 
 
 my $best = (sort {$a<=>$b} keys %assembly)[0];
 `mv test$best k$best`;
 #`rm -r test*`;
 `ln -s k$best/contigs.fa .`;
+
+my $exp_cov = $expCov{$best};
+my $cov_cut = $covCut{$best};
+
 
 push @kmers,$best;
 push @expCov,$exp_cov;
