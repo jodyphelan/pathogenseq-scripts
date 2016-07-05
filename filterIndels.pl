e3de95f5a80ac175d2973273215affe356e5be3c
root <root@plum-s1.lshtm.ac.uk>
2016-07-05 14:30:38 +0100

    a

--- filterIndels.pl
+++ filterIndels.pl
@@ -45,9 +45,9 @@ $refFile = abs_path($refFile);
 
 parseVCF($sampleFile,$baseDir);
 
-my $err = `cat $sampleFile | xargs -i -P20 sh -c "verifyIndels.pl {} $baseDir $refFile $alg"`;
-print  $err;
-parseAssemblyResults($sampleFile);
+#my $err = `cat $sampleFile | xargs -i -P20 sh -c "verifyIndels.pl {} $baseDir $refFile $alg"`;
+#print  $err;
+#parseAssemblyResults($sampleFile);
 
 
 
